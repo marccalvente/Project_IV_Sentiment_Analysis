36f0cc584a48815fc2987905be7ca412e5433c71
Marc Calvente <marc.calvente.lozano@gmail.com>
2022-11-17 00:31:55 +0100

    Project update

--- main.py
+++ main.py
@@ -21,16 +21,24 @@ def readme ():
 def sql ():
     return jsonify(esecuele.get_everything())
 
-@app.route("/sql/<name>", )
-def lines_from_characters (name):
-    return jsonify(esecuele.get_everything_from_character(name))
+@app.route("/sql/name/<username>", )
+def messages_from_user (username):
+    return jsonify(esecuele.get_everything_from_user(username))
 
 
-@app.route("/sa/<name>/", )
-def sa_from_character (name):
-    everything = esecuele.get_just_dialogue(name)
-    #return jsonify(everything)
-    return jsonify([sia.polarity_scores(i["dialogue"])["compound"] for i in everything])
+@app.route("/sql/date/<date>/", )
+def messages_from_date (date):
+    everything = esecuele.get_messages_from_date(date)
+    return jsonify(everything)
+
+@app.route("/sql/messages_score")
+def messages_score ():
+    return jsonify(esecuele.get_messages_score())
+
+
+@app.route("/sql/emojis_score")
+def emojis_score ():
+    return jsonify(esecuele.get_emojis_score())
 
 
 ####### POST
@@ -38,14 +46,21 @@ def sa_from_character (name):
 def try_post ():
     # Decoding params
     my_params = request.args
-    scene = my_params["scene"]
-    character_name = my_params["character_name"]
-    dialogue = my_params["dialogue"]
+    id = my_params.name
+    date = my_params["date"]
+    username = my_params["username"]
+    message = my_params["message"]
+    emojis = my_params["emojis"]
+    message_translated = my_params["message_translated"]
+    message_score = my_params["message_score"]
+    emojis_score = my_params["emojis_score"]
 
-    # Passing to my function: do the inserr
-    esecuele.insert_one_row(scene, character_name, dialogue)
+
+    esecuele.insert_one_row(id,date, username, message, emojis, message_translated, message_score, emojis_score)
     return f"Query succesfully inserted"
 
 
 if __name__ == "__main__":
     app.run(port=9000, debug=True)
+
+
--- tools/sql_queries.py
+++ tools/sql_queries.py
@@ -2,32 +2,54 @@ from config.sql_connection import engine
 import pandas as pd
 
 def get_everything ():
-    query = """SELECT * FROM users;"""
+    query = """SELECT * FROM chats;"""
     df = pd.read_sql_query(query, engine)
     return df.to_dict(orient="records")
 
-def get_everything_from_character (name):
+def get_everything_from_user (username):
     query = f"""SELECT * 
-    FROM users
-    WHERE character_name = '{name}';"""
+    FROM chats
+    WHERE username = '{username}';"""
 
     df = pd.read_sql_query(query, engine)
     return df.to_dict(orient="records")
 
-def get_just_dialogue (name):
-    query = f"""SELECT dialogue 
-    FROM users
-    WHERE character_name = '{name}';"""
+
+def get_messages_from_date (date):
+    query = f"""SELECT * 
+    FROM chats
+    WHERE date like '{date}%';"""
 
     df = pd.read_sql_query(query, engine)
     return df.to_dict(orient="records")
 
-def insert_one_row (scene, character_name, dialogue):
-    query = f"""INSERT INTO users
-     (scene, character_name, dialogue) 
-        VALUES ({scene}, '{character_name}', '{dialogue}');
+def get_messages_score ():
+    query = f"""SELECT username, AVG(message_score)
+    FROM chats
+    GROUP BY username
+    ORDER BY AVG(message_score) DESC;"""
+
+    df = pd.read_sql_query(query, engine)
+    return df.to_dict(orient="records")
+
+
+def get_emojis_score ():
+    query = f"""SELECT username, AVG(emojis_score)
+    FROM chats
+    GROUP BY username
+    ORDER BY AVG(emojis_score) DESC;"""
+
+    df = pd.read_sql_query(query, engine)
+    return df.to_dict(orient="records")
+
+
+def insert_one_row (id,date, username, message, emojis, message_translated, message_score, emojis_score):
+    query = f"""INSERT INTO chats
+     (id,date,username,message,emojis,message_translated,message_score,emojis_score) 
+        VALUES ({id}, '{date}', '{username}', '{message}', '{emojis}', '{message_translated}', {message_score}, {emojis_score});
     """
     engine.execute(query)
     return f"Correctly introduced!"
 
 
+    
